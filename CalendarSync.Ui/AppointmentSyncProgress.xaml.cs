d1545e9939ffe0dcc6621c03b6ae96d8b0483322
Kev Hunter <MrKevHunter@gmail.com>
2010-06-23 20:46:11 +0100

    Added output of sync to form

--- CalendarSync.Ui/AppointmentSyncProgress.xaml.cs
+++ CalendarSync.Ui/AppointmentSyncProgress.xaml.cs
@@ -27,11 +27,28 @@ namespace CalendarSync.Ui
 			_appointmentSyncEventAggregator = appointmentSyncEventAggregator;
 			InitializeComponent();
 			_appointmentSyncEventAggregator.AppointmentSync+=AppointmentSyncEventAggregatorAppointmentSync;
+
+			dataGrid1.Columns.Add(new DataGridTextColumn());
+			((DataGridTextColumn)dataGrid1.Columns[0]).Binding = new Binding("Item.Title");
+			dataGrid1.Columns[0].Header = "Title";
+
+			dataGrid1.Columns.Add(new DataGridTextColumn());
+			((DataGridTextColumn)dataGrid1.Columns[1]).Binding = new Binding("Item.Start");
+			dataGrid1.Columns[1].Header = "Start Date";
+
+			dataGrid1.Columns.Add(new DataGridTextColumn());
+			((DataGridTextColumn)dataGrid1.Columns[2]).Binding = new Binding("Item.End");
+			dataGrid1.Columns[2].Header = "End Date";
+
+			dataGrid1.Columns.Add(new DataGridTextColumn());
+			((DataGridTextColumn)dataGrid1.Columns[3]).Binding = new Binding("CalendarDestination");
+			dataGrid1.Columns[3].Header = "Calendar Destination";
 		}
 
 		private void AppointmentSyncEventAggregatorAppointmentSync(object sender, AppointmentSyncEventArgs e)
 		{
-			dataGrid1.Items.Add(e.Item);
+			dataGrid1.Items.Add(e);
+			
 		}
 	}
 }
